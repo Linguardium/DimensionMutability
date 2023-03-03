cb031825fd161d4546d1fdf519e572566244da95
Linguardium <linguardium@hobbyist.dev>
2023-03-03 07:57:58 -0600

    Moved world modification and negated modifications of nether and end dimensions

--- src/main/java/mod/linguardium/dimute/mixin/ServerWorldMixin.java
+++ src/main/java/mod/linguardium/dimute/mixin/ServerWorldMixin.java
@@ -17,40 +17,52 @@ import net.minecraft.world.level.LevelProperties;
 import net.minecraft.world.level.ServerWorldProperties;
 import net.minecraft.world.level.UnmodifiableLevelProperties;
 import net.minecraft.world.level.storage.LevelStorage;
+import net.minecraft.world.spawner.Spawner;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.function.Supplier;
 
 @Mixin(ServerWorld.class)
 public abstract class ServerWorldMixin extends World {
 
+    @Mutable
+    @Shadow @Final private ServerWorldProperties worldProperties;
+
+    @Mutable
+    @Shadow @Final private boolean shouldTickTime;
+
     protected ServerWorldMixin(MutableWorldProperties properties, RegistryKey<World> registryRef, RegistryEntry<DimensionType> dimension, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long seed, int maxChainedNeighborUpdates) {
         super(properties, registryRef, dimension, profiler, isClient, debugWorld, seed, maxChainedNeighborUpdates);
     }
 
-    @ModifyVariable(at=@At(value="HEAD"),method="<init>",argsOnly = true)
+    @Inject(at=@At(value="RETURN"),method="<init>")
+    private void modifyServerWorldSettings(MinecraftServer server, Executor workerExecutor, LevelStorage.Session session, ServerWorldProperties properties, RegistryKey<World> worldKey, DimensionOptions dimensionOptions, WorldGenerationProgressListener worldGenerationProgressListener, boolean debugWorld, long seed, List<Spawner> spawners, boolean shouldTickTime, CallbackInfo ci) {
+        if (!debugWorld && !worldKey.getValue().getNamespace().equals("minecraft") ) { // dont modify nether and end dimensions
+            if (this.worldProperties instanceof UnmodifiableLevelProperties) {
+                this.worldProperties = mutableProperties(this.worldProperties);
+            }
+            this.shouldTickTime=true;
+        }
+    }
+
     private static ServerWorldProperties mutableProperties(ServerWorldProperties immutable) {
-        if (immutable instanceof UnmodifiableLevelProperties) {
             ServerWorldProperties baseProperties = ((UnmodifiableLevelPropertiesAccessor)immutable).getWorldProperties();
             if (baseProperties instanceof LevelProperties) {
                 return ((copyableProperties)baseProperties).copy();
             }else {
                 return baseProperties;
             }
-        }
-        return immutable;
     }
-    @ModifyVariable(method="<init>",at = @At("HEAD"),argsOnly = true, ordinal = 1)
-    private static boolean setTicksTime(boolean ticksTime, MinecraftServer server, Executor workerExecutor, LevelStorage.Session session, ServerWorldProperties properties, RegistryKey<World> worldKey, DimensionOptions dimensionOptions, WorldGenerationProgressListener worldGenerationProgressListener, boolean debugWorld) {
-        if (!debugWorld && !worldKey.getValue().getNamespace().equals("minecraft")) {
-            return true;
-        }
-        return ticksTime;
-    }
+
     @Redirect(at=@At(value="INVOKE",target="Lnet/minecraft/server/PlayerManager;sendToAll(Lnet/minecraft/network/Packet;)V"),method="tickWeather")
     private void sendToThisDimension(PlayerManager playerManager, Packet<?> packet) {
         playerManager.sendToDimension(packet, getRegistryKey());
